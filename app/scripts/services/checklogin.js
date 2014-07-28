057b1bccebebce08a39e1ca27a38fd3ebf40031d
rcpeng34 <rcpeng@gmail.com>
2014-07-27 20:10:03 -0700

    Adds login view and common login service

---
+++ app/scripts/services/checklogin.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular.module('beatsMeApp')
+  .factory('checkLogin', function () {
+    // Service logic
+
+    // Public API here
+    return {
+      getToken: function () {
+        var re = /access_token=.*(?=&token_type)/;
+        var dirtyToken = re.exec(window.location.search);
+        if (!dirtyToken) {
+          return undefined
+        } else {
+          return dirtyToken[0].split('=')[1];
+        }
+      },
+      beatsOauth : function(){
+        window.location = 'https://partner.api.beatsmusic.com/v1/oauth2/authorize?'
+          + '&response_type=token'
+          + '&redirect_uri=http://localhost:9000/'
+          + '&client_id=BPMYBFZWBFY84MGF8GEWHG4W';
+      }
+    };
+  });
