0979e377baaac05c6e763efa3d83d6140974e704
rcpeng34 <rcpeng@gmail.com>
2014-07-28 21:04:26 -0700

    Moves beats bam to songqueue service

--- app/scripts/controllers/explore.js
+++ app/scripts/controllers/explore.js
@@ -1,11 +1,13 @@
 'use strict';
 
-var exploreController = function($scope, $http, checkLogin){
+var exploreController = function($scope, $http, checkLogin, songqueue){
   var key = 'bpmybfzwbfy84mgf8gewhg4w';
   if (!checkLogin.getToken()){
       checkLogin.beatsOauth();
     }
   var token = checkLogin.getToken();
+
+  // load featured and editor picks
   var featuredDone = false;
   var ePicksDone = false;
   $scope.highlights = [];
@@ -32,6 +34,7 @@ var exploreController = function($scope, $http, checkLogin){
     .error(function(data){
       console.log('error|', data);
     });
+
   // helper to add tag of playlist or album to each object
   var bothDone = function(){
     if (featuredDone && ePicksDone) {
@@ -45,53 +48,19 @@ var exploreController = function($scope, $http, checkLogin){
       }
     }
   };
+
+  $scope.play = function(beatsID, refObj){
+    // clear queue
+    songqueue.clear();
+    // refObj for playlist has author:obj|tracks:array|user:obj
+    // refObj for album has artist:array|label:obj|tracks:array
+    console.log('inside explore play');
+    for (var i = 0; i < refObj.tracks.length; i++){
+      songqueue.add(refObj.tracks[i].id);
+    }
+    songqueue.load();
+  };
 };
 
 angular.module('beatsMeApp')
-  .controller('ExploreCtrl', ['$scope', '$http', 'checkLogin', exploreController]);
-
-
-
-/* begin code that plays Passenger - Let Her go
-note that the token and variables must be strings in an array as noted below
-
-var bam = new BeatsAudioManager("myBeatsPlayer");
-bam.on("ready", handleReady);
-bam.on("error", handleError);
-function handleReady(value) { 
-    bam.clientId = ['BPMYBFZWBFY84MGF8GEWHG4W'];
-    bam.authentication = {
-        access_token:['fk6yt97tusxke9gn8bf7udv5'], 
-        user_id:['R_Peng']
-    };
-    bam.identifier = ['tr72326023'];
-    bam.load();
-};
-function handleError(value) {
-    console.log("Error: " + value);
-    switch(value){
-        case "auth":
-        // Beats Music API auth error (401)
-        break;
-        case "connectionfailure":
-        // audio stream connection failure
-        break;
-        case "apisecurity":
-        // Beats Music API crossdomain error
-        break;
-        case "streamsecurity":
-        // audio stream crossdomain error
-        break;
-        case "streamio":
-        // audio stream io error
-        break;
-        case "apiio":
-        // Beats Music API io error getting track data
-        break;
-        case "flashversion":
-        // flash version too low or not installed
-        break;
-    }
-};
-
-*/
+  .controller('ExploreCtrl', ['$scope', '$http', 'checkLogin', 'songqueue', exploreController]);
--- app/scripts/controllers/player.js
+++ app/scripts/controllers/player.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var PlayerController = function ($scope, checkLogin) {
+var PlayerController = function ($scope, checkLogin, songqueue) {
   // set token and key
   var key = 'bpmybfzwbfy84mgf8gewhg4w';
   if (!checkLogin.getToken()){
@@ -8,54 +8,14 @@ var PlayerController = function ($scope, checkLogin) {
     }
   var token = checkLogin.getToken();
 
-
-  var bam = new BeatsAudioManager("myBeatsPlayer");
-    bam.on("ready", handleReady);
-    bam.on("error", handleError);
-    function handleReady(value) { 
-        bam.clientId = [key];
-        bam.authentication = {
-            access_token:[token]
-            // , 
-            // user_id:['R_Peng']
-        };
-        bam.identifier = ['tr72326023'];
-        bam.load();
-    };
-    function handleError(value) {
-        console.log("Error: " + value);
-        switch(value){
-            case "auth":
-            // Beats Music API auth error (401)
-            break;
-            case "connectionfailure":
-            // audio stream connection failure
-            break;
-            case "apisecurity":
-            // Beats Music API crossdomain error
-            break;
-            case "streamsecurity":
-            // audio stream crossdomain error
-            break;
-            case "streamio":
-            // audio stream io error
-            break;
-            case "apiio":
-            // Beats Music API io error getting track data
-            break;
-            case "flashversion":
-            // flash version too low or not installed
-            break;
-        }
-    };
   $scope.play = function(){
-    console.log('playing');
+    songqueue.play();
   };
   $scope.pause = function(){
-    console.log('paused');
+    songqueue.pause();
   };
 };
 
 
 angular.module('beatsMeApp')
-  .controller('PlayerCtrl', ['$scope', 'checkLogin', PlayerController]);
+  .controller('PlayerCtrl', ['$scope', 'checkLogin', 'songqueue', PlayerController]);
