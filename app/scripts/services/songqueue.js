2cc59e02d918fb4001c19226a2a2392bd0510308
rcpeng34 <rcpeng@gmail.com>
2014-07-28 21:01:34 -0700

    Adds songqueue

---
+++ app/scripts/services/songqueue.js
@@ -0,0 +1,76 @@
+'use strict';
+
+var songqueue = function(checkLogin){
+  var playlist = [];
+  var key = 'bpmybfzwbfy84mgf8gewhg4w';
+  if (!checkLogin.getToken()){
+      checkLogin.beatsOauth();
+    }
+  var token = checkLogin.getToken();
+
+  var bam = new BeatsAudioManager('myBeatsPlayer');
+  bam.on("ready", handleReady);
+  bam.on("error", handleError);
+  function handleReady(value) {
+      bam.clientId = [key];
+      bam.authentication = {
+          access_token:[token]
+      };
+      bam.load('tr92961795');
+  }
+  function handleError(value) {
+      console.log("Error: " + value);
+      switch(value){
+          case "auth":
+          // Beats Music API auth error (401)
+          break;
+          case "connectionfailure":
+          // audio stream connection failure
+          break;
+          case "apisecurity":
+          // Beats Music API crossdomain error
+          break;
+          case "streamsecurity":
+          // audio stream crossdomain error
+          break;
+          case "streamio":
+          // audio stream io error
+          break;
+          case "apiio":
+          // Beats Music API io error getting track data
+          break;
+          case "flashversion":
+          // flash version too low or not installed
+          break;
+      }
+  }
+
+  return {
+    add: function(beatsID){
+      playlist.push(beatsID);
+    },
+    load: function(){
+      console.log('inside songqueue load');
+      console.log(playlist);
+      if (playlist.length > 0){
+        bam.identifier = playlist.shift();
+        console.log('bam identifier', bam.identifier);
+        bam.load();
+      }
+    },
+    pause: function(){
+      console.log('inside sonqueue pause');
+      bam.pause();
+    },
+    play: function(){
+      console.log('inside songqueue play');
+      bam.play();
+    },
+    clear: function(){
+      playlist = [];
+    }
+  };
+};
+
+angular.module('beatsMeApp')
+  .factory('songqueue', ['checkLogin', songqueue]);
