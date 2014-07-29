505a295f54752724af484436001085ea8634d1f9
rcpeng34 <rcpeng@gmail.com>
2014-07-29 13:52:43 -0700

    Moves userID get request into factory and stores

--- app/scripts/services/checklogin.js
+++ app/scripts/services/checklogin.js
@@ -1,23 +1,42 @@
 'use strict';
 
 angular.module('beatsMeApp')
-  .factory('checkLogin', function () {
+  .factory('checkLogin', ['$http', function ($http) {
+    var retrieveToken = function () {
+      var re = /access_token=.*(?=&token_type)/;
+      var dirtyToken = re.exec(window.location.search);
+      if (!dirtyToken) {
+        return undefined;
+      } else {
+        return dirtyToken[0].split('=')[1];
+      }
+    };
+
+    var token = retrieveToken();
+    var userID = null;
+
+   $http({method: 'GET', url: 'https://partner.api.beatsmusic.com/v1/api/me?access_token=' + retrieveToken()})
+    .success(function(response){
+      userID = response.result.user_context;
+    })
+    .error(function(response, status){
+      console.log('error in getUser http request status ', status, '|', response);
+    });
+
+
     // Public API here
     return {
-      getToken: function () {
-        var re = /access_token=.*(?=&token_type)/;
-        var dirtyToken = re.exec(window.location.search);
-        if (!dirtyToken) {
-          return undefined
-        } else {
-          return dirtyToken[0].split('=')[1];
-        }
+      getToken : function () {
+        return token;
       },
       beatsOauth : function(){
         window.location = 'https://partner.api.beatsmusic.com/v1/oauth2/authorize?'
           + '&response_type=token'
           + '&redirect_uri=http://localhost:9000/'
           + '&client_id=BPMYBFZWBFY84MGF8GEWHG4W';
+      },
+      getUserID : function(){
+        return userID;
       }
     };
-  });
+  }]);
