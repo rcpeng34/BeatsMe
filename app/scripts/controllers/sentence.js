560428d3d493fe9cfd9a44f94e32dd2d66923a92
rcpeng34 <rcpeng@gmail.com>
2014-07-29 13:51:34 -0700

    Displays userID

--- app/scripts/controllers/sentence.js
+++ app/scripts/controllers/sentence.js
@@ -1,11 +1,11 @@
 'use strict';
 
-var sentenceController = function ($scope){
+var sentenceController = function ($scope, checkLogin){
   $scope.imPhrases = ['being awesome', 'sleepy', 'poopy'];
   $scope.imSentence = $scope.imPhrases[0];
   $scope.feelingPhrases = ['meh', 'bah', 'lol'];
-  
+  $scope.userID = checkLogin.getUserID();
 };
 
 angular.module('beatsMeApp')
-  .controller('SentenceCtrl', ['$scope', sentenceController]);
+  .controller('SentenceCtrl', ['$scope', 'checkLogin', sentenceController]);
