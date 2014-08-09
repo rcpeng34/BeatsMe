64560f48ed5f4b62a85c7327d7fc8c577cf0501a
rcpeng34 <rcpeng@gmail.com>
2014-08-09 10:24:20 -0700

    Removes serverside id call

--- server.js
+++ server.js
@@ -11,34 +11,16 @@ var server = app.listen(httpport, function() {
     console.log('Listening on port %d', '|', server.address().port, '|' + typeof(server.address().port));
 });
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
+
 // Serve up content from public directory
 app.use('/', express.static(__dirname + '/app'));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
-
-// get request handlers
-app.use('/beatsID/:id', function(req, res) {
-
-  var options = {
-    hostname: 'partner.api.beatsmusic.com',
-    path: '/v1/api/me?access_token=' + req.params.id + '&client_id=BPMYBFZWBFY84MGF8GEWHG4W',
-    method: 'GET'
-  };
-
-  https.get(options, function(response){
-    console.log(response.statusCode);
-    response.setEncoding('utf8');
-    response.on('data', function(beatsRes){
-      if (response.statusCode === 200){
-        res.send(beatsRes);
-      } else {
-        res.status(response.statusCode).send('error making https to beatsmusic for /me');
-      }
-    });
-
-  });
-
-});
 app.get('/', function(req, res) {
   res.sendfile(__dirname + '/app/index.html');
 });
